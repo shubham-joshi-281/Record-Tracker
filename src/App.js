c51000e8b11a164827ebba5d45624ade8d95dec3
Shubham <shubhamjoshi281@gmail.com>
2023-02-24 08:33:33 +0530

    Src / App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Header from "./Components/Header";
+import './Components/Header.css'
+import './Components/Navbar.css'
+import './Components/Note.css'
+import './App.css'
+import Navbar from "./Components/Navbar";
+import Note from "./Components/Note";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="section-1">
+        <Navbar />
+        <Note />
+      </div>
+    </>
   );
 }
 
